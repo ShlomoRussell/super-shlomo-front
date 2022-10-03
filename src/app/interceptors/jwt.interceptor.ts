e8965508a701db03b12172bf0ddb1cb8d8bedfe3
bearddude1 <baapteshuva@gmail.com>
2022-10-04 00:36:18 +0300

    added base url

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,27 +1,32 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  constructor() {}
 
-  constructor() { }
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     let _request;
-    const jwtStorage = window.localStorage.getItem(environment.jwtLocalStorageKey);
+    const jwtStorage = window.localStorage.getItem(
+      environment.jwtLocalStorageKey
+    );
     if (jwtStorage) {
       _request = request.clone({
+        url: 'https://super-shlomo-api.onrender.com',
         setHeaders: {
-          'Authorization': 'bearer ' + jwtStorage
-        }
-      })
+          Authorization: 'bearer ' + jwtStorage,
+        },
+      });
     }
     return next.handle(_request || request);
   }
