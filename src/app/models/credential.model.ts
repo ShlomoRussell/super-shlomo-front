c784f981502e624c5b0974e6f4a621fe79250304
bearddude1 <baapteshuva@gmail.com>
2022-10-12 21:18:49 +0300

    removed email

--- src/app/models/credential.model.ts
+++ src/app/models/credential.model.ts
@@ -1,5 +1,5 @@
 export class LoginCredentials {
-  constructor(public email?: string, public username?: string, public password?: string) {}
+  constructor(public username?: string, public password?: string) { }
 }
 
 
