5cadbe16b7203d95359c7056e8f90339faf43dd9
bearddude1 <baapteshuva@gmail.com>
2022-10-20 10:58:40 +0300

    finish currentSiteInfo

--- src/app/components/current-site-info/current-site-info.component.ts
+++ src/app/components/current-site-info/current-site-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { ItemsService } from 'src/app/services/items.service';
+import { OrderService } from 'src/app/services/order.service';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-current-site-info',
@@ -8,12 +10,28 @@ import { ItemsService } from 'src/app/services/items.service';
   styleUrls: ['./current-site-info.component.css'],
 })
 export class CurrentSiteInfoComponent implements OnInit {
-  constructor(private router: Router) {}
+  public numberOfItemsAvailable!: number
+  public numberOfOrdersPlaced!: number
+  public dateOfLastPurchase!: Date
+  public openCart!: Date
+  public today = new Date()
+  public cartIsEmpty = false
+  public isLoggedIn = false
+  constructor(
+    private itemsService: ItemsService,
+    private shoppingCartService: ShoppingCartService,
+    private ordersService: OrderService,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.itemsService.getItems.subscribe(res => this.numberOfItemsAvailable = res.length)
+    this.ordersService.getCountOfOrders().subscribe(res => this.numberOfOrdersPlaced = res)
+    this.ordersService.getLatestOrder().subscribe(res => this.dateOfLastPurchase = res.orderedDate!)
+    this.authService.getLoggedIn.subscribe(res => this.isLoggedIn = res)
+    this.shoppingCartService.getCart.subscribe(res => {
+      this.openCart = new Date(res.dateCreated!)
+      this.cartIsEmpty = !!res.items.length
+    })
+  }
 
-  }
-  public startShopping() {
-    this.router.navigate(['/shop']);
-  }
 }
