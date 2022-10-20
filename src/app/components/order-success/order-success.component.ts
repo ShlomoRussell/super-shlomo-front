849853b585c40b29457b9d100c6462169aba0eaa
bearddude1 <baapteshuva@gmail.com>
2022-10-21 00:16:43 +0300

    added receipt file

--- src/app/components/order-success/order-success.component.ts
+++ src/app/components/order-success/order-success.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, Input, OnInit, } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import OrderModel from 'src/app/models/order.model';
 import { CartItemMapped } from 'src/app/models/shoppingCart.model';
 import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { OrderService } from 'src/app/services/order.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-order-success',
@@ -17,17 +19,25 @@ export class OrderSuccessComponent implements OnInit {
   @Input()
   cartItems!: CartItemMapped[];
   public tax!: string;
+  public fileUrl!: SafeResourceUrl;
   constructor(
     public activeModal: NgbActiveModal,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private orderService: OrderService,
+    private sanitizer: DomSanitizer,
+  ) { }
 
   ngOnInit(): void {
     this.authService.getUser.subscribe((res) => (this.user = res));
     this.tax = (Number(this.order.totalPrice!) * 0.17).toFixed(2);
+    const data = this.cartItems.reduce((prev, curr) =>
+      prev + `${curr.productName} (Qty:${curr.quantity}) - price: ${curr.price} - total: ${curr.totalPrice}\n`, '') + `Tax: ${this.tax}\n` + `Total: ${this.order.totalPrice}`
+    const blob = new Blob([data], { type: 'application/pdf' });
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
   }
 
-  closeModal(sendData: string) {
+
+  public closeModal(sendData: string) {
     this.activeModal.close(sendData);
   }
 }
