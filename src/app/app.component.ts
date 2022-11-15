dab162da281e8c8f502b32faa9f596e928e7f65c
bearddude1 <baapteshuva@gmail.com>
2022-11-15 23:36:30 +0200

    token unsub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,24 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  private token = false;
   constructor(private authService: AuthService) { }
   ngOnInit(): void {
-    const result = this.authService
-      .checkToken()
-      .subscribe({
-        next: (res) => {
-          this.authService.setUser(res)
-          result.unsubscribe();
-        },
-        error(err) { },
-      })
+    const tokenSub = this.authService.getToken.subscribe(res => {
+      this.token = res;
+      tokenSub.unsubscribe();
+    })
+    if (this.token) {
+      const result = this.authService
+        .checkToken()
+        .subscribe({
+          next: (res) => {
+            this.authService.setUser(res)
+            result.unsubscribe();
+          },
+          error(err) { },
+        })
+    }
   }
   title = 'client';
 }
